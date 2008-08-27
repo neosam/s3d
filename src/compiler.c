784d16583718a02256ca55adf107aa25edb3be28
Simon Goller <neosam@gmail.com>
2008-08-27 21:01:11 +0200

    Compiler can create the stream head

--- src/compiler.c
+++ src/compiler.c
@@ -18,11 +18,67 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+
 #include "compiler.h"
+#include "sha1.h"
 #include "misc.h"
 
+
+/*
+ * The Head of the stream object:
+ * | author-length | author-name | date | parent |
+ * | 8 bit number  |    string   | 32bit| 20byte |
+ */
+int writeHead(char *stream, char *author, char *parent)
+{
+	int aleni = strlen(author);
+	char *p = stream + 20; /* There is a sha1 before the head */
+	unsigned int date = time(NULL);
+	
+	if (aleni > 256)
+		return -1;
+	
+	/* Set author length */
+	*(p++) = aleni;
+	
+	/* Set author */
+	strcpy(p, author);
+	p += aleni;
+
+	/* Set date TODO: ENDIAN */
+	memcpy(p, &date, 4);
+	p += 4;
+
+	/* Set parent */
+	memcpy(p, parent, 20);
+
+	/* Length = author length + author + date + parent */
+	return 1 + aleni + 8 + 20;
+}
+
+int insertSha1(char *stream, int size)
+{
+	SHA_CTX sha;
+	SHA1_Init(&sha);
+	SHA1_Update(&sha, stream+20, size);
+	SHA1_Final(stream, &sha);
+}
 
 char *compile(char *code, char *author, char *parent)
 {
 	char *stream = MALLOCN(char, 4096);
+	int headsize, datasize;
+
+	if ((headsize = writeHead(stream, author, parent)) < 0)
+		return NULL;
+
+	/* Compile data is coming soon */
+	datasize = 0;
+
+	insertSha1(stream, headsize + datasize);
+
+	return stream;
 }
