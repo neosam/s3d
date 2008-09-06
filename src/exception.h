2cd7fe08033723c5871275cb19ff39a7c1779ad9
Simon Goller <neosam@gmail.com>
2008-09-06 17:15:10 +0200

    Add exception handling

---
+++ src/exception.h
@@ -0,0 +1,38 @@
+#ifndef EXCEPTION_H
+#define EXCEPTION_H
+
+#include <setjmp.h>
+
+jmp_buf jumper[256];
+int excpos = 0;
+int exclast;
+const char* excmsg;
+
+#define THROW(exc) { exclast = exc; longjmp(jumper[excpos--], exc); }
+#define THROWDOWN THROW(exclast)
+#define THROWS(exc, str)                                                 \
+{                                                                        \
+	excmsg = str;                                                    \
+        THROW(exc);                                                      \
+}      
+#define THROWIF(expression, exc, str)                                    \
+	if (expression)							 \
+		THROWS(exc, str)
+
+#define TRY                                                              \
+switch (setjmp(jumper[++excpos])) {                                      \
+case 0:
+
+#define CATCHIF(exc)                                                     \
+excpos--;								 \
+break;                                                                   \
+case exc:
+
+#define CATCH                                                            \
+excpos--;                                                                \
+break;                                                                   \
+default:
+
+#define TRYEND excpos--; break;} 
+
+#endif
